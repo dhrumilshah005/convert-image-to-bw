fb3757792f53f751cc8c9c4fbc63b0d0763c0c5f
Dhrumil Shah <dhrumilshah2007@gmail.com>
2024-01-26 12:21:53 +0530

    Initial Commit

---
+++ webapp.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+st.title("Convert an Image to Black and White")
+with st.expander("Start Camera"):
+    camera_image = st.camera_input("camera")
+if camera_image:
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+    st.write("Black and White image")
+    st.image(gray_img)
